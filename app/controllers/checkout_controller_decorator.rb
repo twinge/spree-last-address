184495472eb4c00e0639f360dacda4f29727c004
Josh Starcher <josh.starcher@gmail.com>
2012-06-04 17:53:39 -0400

    use Spree:: scope

--- app/controllers/checkout_controller_decorator.rb
+++ app/controllers/checkout_controller_decorator.rb
@@ -10,8 +10,8 @@ Spree::CheckoutController.class_eval do
     # in case none found
     @order.bill_address ||= current_user.bill_address if current_user
     @order.ship_address ||= current_user.ship_address if current_user
-    @order.bill_address ||= Address.new(:country_id => Spree::Config[:default_country_id])
-    @order.ship_address ||= Address.new(:country_id => Spree::Config[:default_country_id])
+    @order.bill_address ||= Spree::Address.new(:country_id => Spree::Config[:default_country_id])
+    @order.ship_address ||= Spree::Address.new(:country_id => Spree::Config[:default_country_id])
   end
 
 
